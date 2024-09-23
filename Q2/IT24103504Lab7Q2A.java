58b25e592b0e24ea8224608cc7c91ae369a28120
IT24103504 <it24103504@my.sliit.lk>
2024-09-23 20:54:06 +0530

    Add IT24103504Lab7Q2A.java to Q2A

---
+++ Q2/IT24103504Lab7Q2A.java
@@ -0,0 +1,19 @@
+
+public class IT24103504Lab7Q2A {
+    public static void main(String[] args) {
+
+int i,j;
+       
+        for (i = 1; i < 5; i++) {
+        
+
+            for (j = 1; j < 6; j++) {
+                System.out.print("$ ");
+            }
+           
+            System.out.println();
+        }
+    
+ }
+
+}
