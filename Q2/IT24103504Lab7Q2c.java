9f35915f4638f1fc0a7ced18e74e4bf192f51214
IT24103504 <it24103504@my.sliit.lk>
2024-09-23 22:44:50 +0530

    Add IT24103504Lab7Q2c.java to Q2c

---
+++ Q2/IT24103504Lab7Q2c.java
@@ -0,0 +1,16 @@
+
+public class IT24103504Lab7Q2c {
+    public static void main(String[] args) {
+        
+             for (int i = 5; i >= 1; i--) {
+                 
+                  
+                      for (int j = 1; j <= i; j++) {
+               
+                              System.out.print(i);
+                                                    }
+          
+                      System.out.println();
+          }
+    }
+}
